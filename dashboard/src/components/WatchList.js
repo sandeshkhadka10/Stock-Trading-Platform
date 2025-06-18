6a16f702f7a58211cb3fcfe9a6006f28417abc4c
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2025-06-18 17:27:26 +0545

    display the data for the watchlist component

--- dashboard/src/components/WatchList.js
+++ dashboard/src/components/WatchList.js
@@ -1,8 +1,11 @@
 import React,{useState} from "react";
 
 import {Tooltip, Grow} from '@mui/material';
+import {KeyboardArrowDown, KeyboardArrowUp} from "@mui/icons-material";
 
 import {watchlist} from "../data/data";
+
+
 
 const WatchList = () => {
   return (
@@ -20,7 +23,9 @@ const WatchList = () => {
 
       <ul className="list">
         {watchlist.map((stock,index)=>{
-          <WatchListItem stock={stock} key={index}/>
+          return(
+            <WatchListItem stock={stock} key={index}/>
+          )
         })}
       </ul>
     </div>
@@ -28,3 +33,28 @@ const WatchList = () => {
 };
 
 export default WatchList;
+
+const WatchListItem = ({stock}) =>{
+  const [showWatchListActions, setShowWatchListActions] = useState(false);
+
+  const handleMouseEnter = (e)=>{
+    setShowWatchListActions(true);
+  }
+
+  const handleMouseLeave = (e)=>{
+    setShowWatchListActions(false);
+  }
+
+  return(
+    <li onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+      <div className="item">
+        <p className={stock.isDown ? "down":"up"}>{stock.name}</p>
+        <div className="itemInfo">
+          <span className="percent">{stock.percent}</span>
+          {stock.isDown ? (<KeyboardArrowDown className="down"/>) : (<KeyboardArrowUp className="up"/>) }
+          <span className="price">{stock.price}</span>
+        </div>
+      </div>
+    </li>
+  )
+}
