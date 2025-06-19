29756a911e9e823d2d6d2fd0ce6fd9fa2a8f1e5d
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2025-06-19 19:53:10 +0545

    commented the code of inserting the dummy data

--- backend/index.js
+++ backend/index.js
@@ -144,41 +144,42 @@ app.listen(3002, () => {
 //   res.send("Done");
 // });
 
-app.get("/addPositions", (req, res) => {
-  let tempData = [
-    {
-      product: "CNC",
-      name: "EVEREADY",
-      qty: 2,
-      avg: 316.27,
-      price: 312.35,
-      net: "+0.58%",
-      day: "-1.24%",
-      isLoss: true,
-    },
-    {
-      product: "CNC",
-      name: "JUBLFOOD",
-      qty: 1,
-      avg: 3124.75,
-      price: 3082.65,
-      net: "+10.04%",
-      day: "-1.35%",
-      isLoss: true,
-    },
-  ];
-  tempData.forEach((item) => {
-    let newPosition = new PositionsModel({
-      product: item.product,
-      name: item.name,
-      qty: item.qty,
-      avg: item.avg,
-      price: item.price,
-      net: item.net,
-      day: item.day,
-      isLoss: item.isLoss,
-    });
-    newPosition.save();
-  });
-  res.send("Done");
-});
+// adding the dummy data for positions in the database
+// app.get("/addPositions", (req, res) => {
+//   let tempData = [
+//     {
+//       product: "CNC",
+//       name: "EVEREADY",
+//       qty: 2,
+//       avg: 316.27,
+//       price: 312.35,
+//       net: "+0.58%",
+//       day: "-1.24%",
+//       isLoss: true,
+//     },
+//     {
+//       product: "CNC",
+//       name: "JUBLFOOD",
+//       qty: 1,
+//       avg: 3124.75,
+//       price: 3082.65,
+//       net: "+10.04%",
+//       day: "-1.35%",
+//       isLoss: true,
+//     },
+//   ];
+//   tempData.forEach((item) => {
+//     let newPosition = new PositionsModel({
+//       product: item.product,
+//       name: item.name,
+//       qty: item.qty,
+//       avg: item.avg,
+//       price: item.price,
+//       net: item.net,
+//       day: item.day,
+//       isLoss: item.isLoss,
+//     });
+//     newPosition.save();
+//   });
+//   res.send("Done");
+// });
