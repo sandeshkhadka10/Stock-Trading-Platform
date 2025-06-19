442cca5e69adf783a8bc93236c464ee5d7147543
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2025-06-19 13:31:58 +0545

    set the port and mongodb url

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,16 @@
+require('dotenv').config();
+
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 
+// it is for the deployment
+const PORT = process.env.PORT || 3002;
+
+const url = process.env.MONGO_URL;
 
 app.listen(3002,()=>{
     console.log("App started!");
+    mongoose.connect(url);
+    console.log("Database connected");
 });
