671f10677e36a27ba0bf0acd4afba45dd59c1805
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2025-06-23 22:46:20 +0545

    edit button not working

--- dashboard/src/components/EditActionWindow.js
+++ dashboard/src/components/EditActionWindow.js
@@ -1,78 +1,76 @@
-// import React, { useState, useContext } from "react";
-// import { Link } from "react-router-dom";
+import React, { useState, useContext } from "react";
+import { Link } from "react-router-dom";
 
-// import "./EditActionWindow.css";
+import "./EditActionWindow.css";
 
-// import axios from "axios";
+import axios from "axios";
 
-// import GeneralContext from "./GeneralContext";
+import GeneralContext from "./GeneralContext";
 
-// const EditActionWindow = ({ uid }) => {
-//   const [stockQuantity, setStockQuantity] = useState(1);
-//   const [stockPrice, setStockPrice] = useState(0.0);
-//   const [allHoldings, setAllHoldings] = useState([]);
+const EditActionWindow = ({ uid }) => {
+  const [editStockQuantity, setEditStockQuantity] = useState("");
+  const [editStockPrice, setEditStockPrice] = useState("");
+  const [allHoldings, setAllHoldings] = useState([]);
 
-//   const handleSellClick = () => {
-//     axios.post("http://localhost:3002/newOrder", {
-//       name: uid,
-//       qty: stockQuantity,
-//       price: stockPrice,
-//       model: "Sell",
-//     })
-//     .then(() => {
-//         axios.get("http://localhost:3002/allHoldings").then((res) => {
-//           setAllHoldings(res.data);
-//         });
-//       });
-//     generalContext.closeSellWindow();
-//   };
+  const handleEditClick = () => {
+    axios.post("http://localhost:3002//editOrder/${editOrder._id}", {
+      name: uid,
+      qty: editStockQuantity,
+      price:editStockPrice
+    })
+    .then(() => {
+        axios.get("http://localhost:3002/allHoldings").then((res) => {
+          setAllHoldings(res.data);
+        });
+      });
+    generalContext.closeSellWindow();
+  };
 
-//   const generalContext = useContext(GeneralContext);
-//   const handleCancelClick = () => {
-//     generalContext.closeSellWindow();
-//   };
+  const generalContext = useContext(GeneralContext);
+  const handleCancelClick = () => {
+    generalContext.closeEditWindow();
+  };
 
-//   return (
-//     <div className="container" id="buy-window" draggable="true">
-//       <div className="regular-order">
-//         <div className="inputs">
-//           <fieldset>
-//             <legend>Qty.</legend>
-//             <input
-//               type="number"
-//               name="qty"
-//               id="qty"
-//               onChange={(e) => setStockQuantity(e.target.value)}
-//               value={stockQuantity}
-//             />
-//           </fieldset>
-//           <fieldset>
-//             <legend>Price</legend>
-//             <input
-//               type="number"
-//               name="price"
-//               id="price"
-//               step="0.05"
-//               onChange={(e) => setStockPrice(e.target.value)}
-//               value={stockPrice}
-//             />
-//           </fieldset>
-//         </div>
-//       </div>
+  return (
+    <div className="container" id="buy-window" draggable="true">
+      <div className="regular-order">
+        <div className="inputs">
+          <fieldset>
+            <legend>Qty.</legend>
+            <input
+              type="number"
+              name="qty" 
+              id="qty"
+              onChange={(e) => setEditStockQuantity(e.target.value)}
+              value={editStockQuantity}
+            />
+          </fieldset>
+          <fieldset>
+            <legend>Price</legend>
+            <input
+              type="number"
+              name="price"
+              id="price"
+              step="0.05"
+              onChange={(e) => setEditStockPrice(e.target.value)}
+              value={editStockPrice}
+            />
+          </fieldset>
+        </div>
+      </div>
 
-//       <div className="buttons">
-//         <span>Margin required ₹140.65</span>
-//         <div>
-//           <Link className="btn btn-blue" onClick={handleSellClick}>
-//             Sell
-//           </Link>
-//           <Link to="" className="btn btn-grey" onClick={handleCancelClick}>
-//             Cancel
-//           </Link>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
+      <div className="buttons">
+        <div>
+          <Link className="btn btn-blue" onClick={handleEditClick}>
+            Apply
+          </Link>
+          <Link to="" className="btn btn-grey" onClick={handleCancelClick}>
+            Cancel
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-// export default EditActionWindow;
+export default EditActionWindow;
