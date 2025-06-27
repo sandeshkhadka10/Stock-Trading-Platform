de6c89a746323bd448672fb45e0d52a6670bf267
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2025-06-27 23:40:17 +0545

    installed the necessary package for doing the signup and login procedure

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import './index.css';
+import 'react-toastify/dist/ReactToastify.css';
 import HomePage from './landing_page/home/HomePage';
 import Signup from './landing_page/signup/Signup';
 import AboutPage from './landing_page/about/AboutPage';
