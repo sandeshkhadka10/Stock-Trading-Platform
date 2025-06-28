da782353194da55deb6da223ed71a4a22f127b99
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2025-06-28 22:19:03 +0545

    work of signup on process for fronted

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import './index.css';
+import 'react-toastify/dist/ReactToastify.css';
 import HomePage from './landing_page/home/HomePage';
+import Signup from './landing_page/signup/Signup';
 import AboutPage from './landing_page/about/AboutPage';
 import ProductPage from './landing_page/products/ProductPage';
 import PricingPage from "./landing_page/pricing/PricingPage";
@@ -10,6 +12,7 @@ import SupportPage from './landing_page/support/SupportPage';
 import Navbar from './landing_page/Navbar';
 import Footer from './landing_page/Footer';
 import NotFound from './landing_page/NotFound';
+import Login from './landing_page/login/Login';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -17,6 +20,8 @@ root.render(
   <Navbar/>
   <Routes>
     <Route path='/' element={<HomePage/>}/>
+    <Route path='/signup' element={<Signup/>}/>
+    <Route path='/login' element={<Login/>}/>
     <Route path='/about' element={<AboutPage/>}/>
     <Route path='/product' element={<ProductPage/>}/>
     <Route path='/pricing' element={<PricingPage/>}/>
--- frontend/src/landing_page/signup/Signup.js
+++ frontend/src/landing_page/signup/Signup.js
@@ -1,9 +1,48 @@
 import React from "react";
+import {useForm} from "react-hook-form";
+import {Link,useNavigate} from "react-router-dom";
+import axios from "axios";
+import {ToastContainer,toast} from "react-toastify";
 
-function Signup(){
-    return(
-        <h1>Signup</h1>
-    )
+const Signup = () => {
+    const navigate = useNavigate();
+
+    const {register,handleSubmit,formState:{errors},reset} = useForm();
+
+
+    const handleError = (err) => {
+        toast.error(err,{
+            position:"bottom-left"
+        });
+    }
+
+    const handleSuccess = (msg) => {
+        toast.success(msg,{
+            position:"bottom-right"
+        });
+    }
+
+    const onSubmit = async(data) => {
+        try{
+            const response = await axios.post("http://localhost:3002/signup",
+                data,
+                {withCredentials: true}
+            );
+            const {success, message} = response.data;
+
+            if(success){
+                handleSuccess(message){
+                    reset();
+                    setTimeout(()=>{})
+                }
+            }
+
+        }catch(error){
+            console.log(error);
+            handleError("Server error. Please try again");
+        }
+    }
+
 }
 
 export default Signup;
