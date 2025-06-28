da00e218fbf656fd313a8f69bf7703e4a7237f34
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2025-06-28 11:48:03 +0545

    remove the unnecessary code

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,9 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import './index.css';
-import 'react-toastify/dist/ReactToastify.css';
 import HomePage from './landing_page/home/HomePage';
-import Signup from './landing_page/signup/Signup';
 import AboutPage from './landing_page/about/AboutPage';
 import ProductPage from './landing_page/products/ProductPage';
 import PricingPage from "./landing_page/pricing/PricingPage";
@@ -19,7 +17,6 @@ root.render(
   <Navbar/>
   <Routes>
     <Route path='/' element={<HomePage/>}/>
-    <Route path='/signup' element={<Signup/>}/>
     <Route path='/about' element={<AboutPage/>}/>
     <Route path='/product' element={<ProductPage/>}/>
     <Route path='/pricing' element={<PricingPage/>}/>
