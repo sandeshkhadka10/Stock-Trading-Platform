3da39ceacea67fee01076d8d15d68eeec7347221
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2025-06-29 19:54:06 +0545

    signup and login completed

--- frontend/src/landing_page/login/Login.js
+++ frontend/src/landing_page/login/Login.js
@@ -1,9 +1,76 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import {Link} from "react-router-dom";
 
-function Login(){
-    return(
-        <h1>Login</h1>
-    )
-}
+const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
+
+  return (
+    <div className="container" style={{ width: "30%" }}>
+      <ToastContainer />
+      <div className="row">
+        <div className="col">
+          <h2 className="text-center mt-3">Login Now</h2>
+          <form>
+            <div className="mb-3 fs-5">
+              <label htmlFor="email" className="form-label">
+                Email
+              </label>
+              <input
+                type="email"
+                className="form-control"
+                id="email"
+                {...register("email", {
+                  required: "Email is required",
+                  pattern: {
+                    value: /^\S+@\S+$/i,
+                    message: "Invalid email format",
+                  },
+                })}
+              ></input>
+              {errors.email && (
+                <p className="text-danger">{errors.email.message}</p>
+              )}
+            </div>
+            <div className="mb-3 fs-5">
+              <label htmlFor="password" className="form-label">
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                className="form-control"
+                {...register("password", {
+                  required: "Password is required",
+                  minLength: {
+                    value: 6,
+                    message: "Password must be at least 6 characters",
+                  },
+                })}
+              ></input>
+              {errors.password && (
+                <p className="text-danger">{errors.password.message}</p>
+              )}
+            </div>
+
+            <div className="text-center mt-3">
+              <button type="submit" className="btn btn-primary fs-5 px-4">
+                Login
+              </button>
+            </div>
+            <span>Already have an account? <Link to={"/signup"}>Signup</Link></span>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Login;
--- frontend/src/landing_page/signup/Signup.js
+++ frontend/src/landing_page/signup/Signup.js
@@ -1,48 +1,95 @@
 import React from "react";
-import {useForm} from "react-hook-form";
-import {Link,useNavigate} from "react-router-dom";
-import axios from "axios";
-import {ToastContainer,toast} from "react-toastify";
+import { useForm } from "react-hook-form";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import {Link} from "react-router-dom";
 
 const Signup = () => {
-    const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
 
-    const {register,handleSubmit,formState:{errors},reset} = useForm();
+  return (
+    <div className="container" style={{ width: "30%" }}>
+      <ToastContainer />
+      <div className="row">
+        <div className="col">
+          <h2 className="text-center mt-3">Signup Now</h2>
+          <form>
+            <div className="mb-3 fs-5">
+              <label htmlFor="fullName" className="form-label">
+                Username
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="fullName"
+                {...register("fullName", {
+                  required: "Username is required required",
+                })}
+              ></input>
+              {errors.fullName && (
+                <p className="text-danger">{errors.fullName.message}</p>
+              )}
+            </div>
 
+            <div className="mb-3 fs-5">
+              <label htmlFor="email" className="form-label">
+                Email
+              </label>
+              <input
+                type="email"
+                className="form-control"
+                id="email"
+                {...register("email", {
+                  required: "Email is required",
+                  pattern: {
+                    value: /^\S+@\S+$/i,
+                    message: "Invalid email format",
+                  },
+                })}
+              ></input>
+              {errors.email && (
+                <p className="text-danger">{errors.email.message}</p>
+              )}
+            </div>
+            <div className="mb-3 fs-5">
+              <label htmlFor="password" className="form-label">
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                className="form-control"
+                {...register("password", {
+                  required: "Password is required",
+                  minLength: {
+                    value: 6,
+                    message: "Password must be at least 6 characters",
+                  },
+                })}
+              ></input>
+              {errors.password && (
+                <p className="text-danger">{errors.password.message}</p>
+              )}
+            </div>
 
-    const handleError = (err) => {
-        toast.error(err,{
-            position:"bottom-left"
-        });
-    }
-
-    const handleSuccess = (msg) => {
-        toast.success(msg,{
-            position:"bottom-right"
-        });
-    }
-
-    const onSubmit = async(data) => {
-        try{
-            const response = await axios.post("http://localhost:3002/signup",
-                data,
-                {withCredentials: true}
-            );
-            const {success, message} = response.data;
-
-            if(success){
-                handleSuccess(message){
-                    reset();
-                    setTimeout(()=>{})
-                }
-            }
-
-        }catch(error){
-            console.log(error);
-            handleError("Server error. Please try again");
-        }
-    }
-
-}
+            <div className="text-center mt-3">
+              <button type="submit" className="btn btn-primary fs-5 px-4">
+                Signup
+              </button>
+            </div>
+            <div className="text-center mt-2">
+                 <span>Already have an account? <Link to={"/login"}>Login</Link></span>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Signup;
