b271b0b9c74a25d82495a596417978d04933e082
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2025-06-29 22:10:05 +0545

    route name added

--- backend/routes/AuthRoute.js
+++ backend/routes/AuthRoute.js
@@ -5,6 +5,6 @@ const router = express.Router();
 
 router.post("/signup",Signup);
 router.post("/login",Login);
-router.post("/",userVerification);
+router.post("/user",userVerification);
 
 module.exports = router;
