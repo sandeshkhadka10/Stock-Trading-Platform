664082c90f4e8626a5fed6c30342acead6e87864
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2025-11-15 23:17:27 +0545

    minor change made

--- backend/schema.js
+++ backend/schema.js
@@ -9,32 +9,32 @@ module.exports.usersSignupSchema = Joi.object({
 
 // for login
 module.exports.usersLoginSchema = Joi.object({
-    email: Joi.string().required(),
+    email: Joi.string().email({ tlds: { allow: false } }).required(),
     password: Joi.string().required()
 });
 
 // for orders
 module.exports.ordersSchema = Joi.object({
     name: Joi.string().required(),
-    qty: Joi.number().min(1).required(),
+    qty: Joi.number().min(1).required().min(1),
     price: Joi.number().required(),
     model:Joi.string().required()
 });
 
 // for edit of orders
 module.exports.editOrdersSchema = Joi.object({
-    qty: Joi.number().min(1).required(),
+    qty: Joi.number().min(1).required().min(1),
     price: Joi.number().min(0.01).required(),
 });
 
 // for forget passowrd
 module.exports.forgetPasswordSchema = Joi.object({
-    email:Joi.string().required()
+    email: Joi.string().email({ tlds: { allow: false } }).required()
 });
 
 // for reset password
 module.exports.resetPasswordSchema = Joi.object({
-    email:Joi.string().required(),
+    email: Joi.string().email({ tlds: { allow: false } }).required(),
     resetCode:Joi.string().required(),
     newPassword:Joi.string().required()
 });
